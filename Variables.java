eecd467b864db87d87bd462fecd41cb577a88006
jathniel Mpondo <jathnielmpondo@gmail.com>
2025-02-20 03:03:57 +0100

    Initial commit

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 3, b = 4;
+        int temp;
+
+        temp = a;
+        a = b;  
+        b = temp;
+
+        System.out.println("a = " + a + "\nb = " + b);
+     
+
+    }
+}
